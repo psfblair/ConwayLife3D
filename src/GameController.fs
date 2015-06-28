c87b682e0dfc0de69ef9535ed2a719ec6e5e75a0
Paul Blair <psfblair@gmail.com>
2015-06-28 16:14:20 -0400

    Refactor out game running into its own class.

--- src/GameController.fs
+++ src/GameController.fs
@@ -4,14 +4,9 @@ open UnityEngine
 open UnityEngine.UI
 open ConwayLife3D
 open ConwayLife3D.Patterns
+open ConwayLife3D.Unity
 open ConwayLife3D.Life.Core
 open System.Collections
-
-type GenerationTransition = Generation * Generation
-type GameState = 
-    | Running 
-    | Restarting
-    | Paused of GenerationTransition * float32
 
 type GameController() = 
     inherit MonoBehaviour()
@@ -26,77 +21,31 @@ type GameController() =
     [<SerializeField>] [<DefaultValue>] val mutable reaper: GameObject
     [<SerializeField>] [<DefaultValue>] val mutable startPanel: GameObject
     [<SerializeField>] [<DefaultValue>] val mutable restartText: Text
-
-    let mutable gameState = Running
+    [<SerializeField>] [<DefaultValue>] val mutable runner: UnityLife
 
     let restartMessage = "Press 'R' to Restart"
 
-    let nextGenerationPairedWithPrevious (previousGeneration, generation): GenerationTransition  =
-        let nextGen = nextGeneration generation
-        (generation, nextGen)
-        
-    let generationSequence (firstGenerationPair: GenerationTransition): seq<GenerationTransition> =
-        Seq.unfold (fun generationPair -> Some(generationPair, nextGenerationPairedWithPrevious generationPair)) firstGenerationPair
-
-    let unityCoordinatesFrom (cell: Cell): Vector3 = 
-        match cell with
-            | (x, y, z)     -> new Vector3(float32 x, float32 y, float32 z)
-
     member this.Start () =
-        gameState <- Running
         this.startPanel.gameObject.SetActive(true)
         this.restartText.text <- restartMessage
 
-    member this.Update () =
-        match gameState with
-            | Restarting ->  Application.LoadLevel(Application.loadedLevel)
-            | Paused(generationPair, pauseStart) when this.PauseWaitElapsed(pauseStart) && Input.GetKey(KeyCode.P) ->
-                Debug.Log("Continuing")
-                gameState <- Running
-                this.StartCoroutine (this.RunGame generationPair) |> ignore
-            | _ -> ()
-
+    // Called on button click
     member this.StartGame (patternName: string) =
         this.startPanel.gameObject.SetActive(false)
         this.restartText.text <- ""
+        let selectedPattern = this.GetSelectedPattern (patternName)
+        this.RunUnityLife (Set.empty, selectedPattern)
+
+    member this.Update () =
+        match this.runner.GameState with
+            | Paused(generationPair, pauseStart) when this.runner.PauseToggled(pauseStart) -> this.RunUnityLife generationPair
+            | Restarting -> Application.LoadLevel(Application.loadedLevel)
+            | _ -> ()
+
+    member private this.GetSelectedPattern (patternName: string): Generation =
         let patternsType = typeof<PatternModuleTypeAccessor>.DeclaringType
-        let selectedPattern = patternsType.GetProperty(patternName).GetValue(null, Array.empty) :?> Generation
-        this.StartCoroutine (this.RunGame (Set.empty, selectedPattern)) |> ignore
+        patternsType.GetProperty(patternName).GetValue(null, Array.empty) :?> Generation
 
-    member private this.RunGame (generationTransition: GenerationTransition): IEnumerator =
-        let initialWait = seq { yield WaitForSeconds(pauseBetweenGenerations) }
-        let waitSequence = generationSequence generationTransition
-                            |> Seq.takeWhile (fun generationPair -> this.ShouldContinue(generationPair))
-                            |> Seq.map (fun generationPair -> this.UpdateSceneAndWait(generationPair)) 
-                            |> Seq.append initialWait
-
-        waitSequence.GetEnumerator() :> IEnumerator
-
-    member private this.UpdateSceneAndWait (previousGeneration: Generation, nextGeneration: Generation) =
-        let cellsToDestroy = previousGeneration - nextGeneration
-        Set.iter this.DeathAt cellsToDestroy
-
-        let cellsToCreate =  nextGeneration - previousGeneration
-        Set.iter this.BirthAt cellsToCreate
-
-        WaitForSeconds(pauseBetweenGenerations)
-
-    member private this.BirthAt (cell: Cell) : Unit = 
-        Object.Instantiate(this.token, (unityCoordinatesFrom cell), Quaternion.identity) |> ignore
-
-    member private this.DeathAt (cell: Cell) : Unit =
-        Object.Instantiate(this.reaper, (unityCoordinatesFrom cell), Quaternion.identity) |> ignore
-
-    member private this.ShouldContinue (generationPair: GenerationTransition) =
-        match gameState with 
-            | Running when Input.GetKey(KeyCode.P) ->
-                Debug.Log("Pausing")
-                gameState <- Paused(generationPair, Time.time)
-                false
-            | Running when Input.GetKey(KeyCode.R) ->
-                Debug.Log("Stopping")
-                gameState <- Restarting
-                false
-            | _ -> true
-
-    member private this.PauseWaitElapsed pauseStart = pauseStart + pauseWait < Time.time
+    member private this.RunUnityLife (generationPair: GenerationTransition) = 
+        this.runner <- new UnityLife(this.token, this.reaper, pauseBetweenGenerations, pauseWait)
+        this.StartCoroutine (this.runner.RunGame(generationPair)) |> ignore
--- src/UnityLife.fs
+++ src/UnityLife.fs
@@ -1,1 +1,68 @@
-﻿
+﻿namespace ConwayLife3D.Unity
+
+open UnityEngine
+open System.Collections
+open ConwayLife3D.Life.Core
+
+type GenerationTransition = Generation * Generation
+type GameState = 
+    | Running of float32
+    | Restarting
+    | Paused of GenerationTransition * float32
+
+type UnityLife(token: GameObject, reaper: GameObject, pauseBetweenGenerations: float32, pauseWait: float32) =
+
+    let mutable gameState = Running Time.time
+
+    let nextGenerationPairedWithPrevious (previousGeneration, generation): GenerationTransition =
+        let nextGen = nextGeneration generation
+        (generation, nextGen)
+
+    let generationSequence (firstGenerationPair: GenerationTransition): seq<GenerationTransition> =
+        Seq.unfold (fun generationPair -> Some(generationPair, nextGenerationPairedWithPrevious generationPair)) firstGenerationPair
+
+    let unityCoordinatesFrom (cell: Cell): Vector3 =
+        match cell with
+            | (x, y, z)     -> new Vector3(float32 x, float32 y, float32 z)
+
+    member this.RunGame (generationTransition: GenerationTransition): IEnumerator =
+        let initialWait = seq { yield WaitForSeconds(pauseBetweenGenerations) }
+        let waitSequence = generationSequence generationTransition
+                            |> Seq.takeWhile (fun generationPair -> this.ShouldContinue(generationPair))
+                            |> Seq.map (fun generationPair -> this.UpdateSceneAndWait(generationPair)) 
+                            |> Seq.append initialWait
+
+        waitSequence.GetEnumerator() :> IEnumerator
+
+    member this.GameState = gameState
+
+    member this.PauseToggled timeOfLastToggle =
+        Input.GetKey(KeyCode.P) && this.PauseWaitElapsed(timeOfLastToggle)
+
+
+    member private this.UpdateSceneAndWait (previousGeneration: Generation, nextGeneration: Generation) =
+        let cellsToDestroy = previousGeneration - nextGeneration
+        Set.iter this.DeathAt cellsToDestroy
+
+        let cellsToCreate =  nextGeneration - previousGeneration
+        Set.iter this.BirthAt cellsToCreate
+
+        WaitForSeconds(pauseBetweenGenerations)
+
+    member private this.BirthAt (cell: Cell) : Unit =
+        Object.Instantiate(token, (unityCoordinatesFrom cell), Quaternion.identity) |> ignore
+
+    member private this.DeathAt (cell: Cell) : Unit =
+        Object.Instantiate(reaper, (unityCoordinatesFrom cell), Quaternion.identity) |> ignore
+
+    member private this.ShouldContinue (generationPair: GenerationTransition) =
+        match gameState with 
+            | Running startTime when this.PauseToggled startTime ->
+                gameState <- Paused(generationPair, Time.time)
+                false
+            | Running startTime when Input.GetKey(KeyCode.R) ->
+                gameState <- Restarting
+                false
+            | _ -> true
+
+    member private this.PauseWaitElapsed timeOfLastToggle = timeOfLastToggle + pauseWait < Time.time
